d169bdfde584efde5f5b2db1881823911d229dd5
deep200315 <deeptanush.kapaka@gmail.com>
2024-05-09 00:27:11 +0530

    nide setup at 9000

--- server/src/db.js
+++ server/src/db.js
@@ -1,11 +1,10 @@
 import {MongoClient} from "mongodb";
-import dotenv from 'dotenv';
-dotenv.config();
+
 
 let db; 
 
 async function connectToDB(cb){
-    const url = process.env.mongodb_url;
+    const url = 'mongodb+srv://bora1132004:Ganesh1907@cluster0.a93zhxx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
     const client = new MongoClient(url);
     await client.connect();
     db = client.db("NFT_MARKET");
